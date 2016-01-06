553ac74bc3dd412d9951b4c948774209e5ca860c
olzhas <olzhas@olzhas-ubuntu>
2016-01-06 10:47:18 +0600

    Added JS code to handle slider's main functionality

--- demo/js/sliderjs.js
+++ demo/js/sliderjs.js
@@ -3,3 +3,38 @@
     Author     : Olzhas Kurenov
 */
 
+var SliderJSCounter = 0;
+var SliderJSTotal = 4;
+var SliderJSPause = false;
+
+
+var SliderJSTimer = setInterval(function () {
+  SliderJS(1);
+}, 3000);
+
+function SliderJSInit() {
+  //TODO: Add code to parse slides and etc
+}
+
+
+function SliderJS(step) {
+  var slideWidth = 280;
+  var limSliderImages = document.getElementById('slider-js--images');
+
+  if (limSliderImages != null) {
+
+    SliderJSCounter = SliderJSCounter + step;
+
+    if (SliderJSCounter >= SliderJSTotal) {
+      SliderJSCounter = 0;
+    }
+    else if (SliderJSCounter < 0) {
+      SliderJSCounter = SliderJSTotal - 1;
+    }
+
+    var pager = document.getElementById("slider-js-footer");
+    pager.innerHTML = (SliderJSCounter + 1) + '/' + SliderJSTotal;
+    var offset = (SliderJSCounter * slideWidth);
+    limSliderImages.style.left = '-' + offset + 'px';
+  }
+}
